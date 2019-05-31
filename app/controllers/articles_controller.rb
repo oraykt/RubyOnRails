25e15696b0cea9ba192e3bc0dd4820ce534f3e2a
OrayKT <oray.kt@gmail.com>
2019-05-31 18:50:40 +0100

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
-
 
   def new
     @article = Article.new
@@ -45,8 +46,12 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
 end
