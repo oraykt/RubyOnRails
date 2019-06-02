9350d873a661ecc5eecc7eb5eb4365b04b24a4c6
OrayKT <oray.kt@gmail.com>
2019-06-02 18:26:27 +0100

    add association between user and articles and debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    # TODO debugger n (next) / strg+D (exit)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
   #   https://guides.rubyonrails.org/active_record_validations.html
   validates :username, presence: true,
-            uniqueness: { case_sensitive: false},
-            length: { minimum: 3, maximum: 25 }
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,
-            uniqueness: { case_sensitive: false },
-            length: { maximum: 100 },
-            format: { with: VALID_EMAIL_REGEX }
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX },
+                    length: { maximum: 100 }
 end
