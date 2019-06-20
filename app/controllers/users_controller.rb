2c84660f3db57508b5fc224579767b5752049c23
OrayKT <oray.kt@gmail.com>
2019-06-20 13:59:53 +0100

    After Signing Up, Redirect to Login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      flash[:success] = "Welcome to the alpha blog #{@user.username}, Please Log in!"
+      redirect_to login_path
     else
       render 'new'
     end
