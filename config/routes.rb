dc1db0e469e237d9fdba36d7dbae41964610b195
OrayKT <oray.kt@gmail.com>
2019-07-23 15:25:41 +0100

    Ruby Routes changed except to only

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  resources :users, except: [:new]
-
+  # resources :users, except: [:new]
+  resources :users, only: [:create, :edit, :index, :update, :show, :destroy]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :categories, except: [:destroy]
-
+  # resources :categories, except: [:destroy]
+  resources :categories, only: [:new, :create, :edit, :update, :show, :index]
 end
